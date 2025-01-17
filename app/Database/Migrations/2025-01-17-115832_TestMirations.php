604578ce478b0302f4f31edd04f4bf6bb7b5f6e7
mglmega <migartr@gmail.com>
2025-01-17 20:19:17 +0800

    first commit

---
+++ app/Database/Migrations/2025-01-17-115832_TestMirations.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TestMirations extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => false,
+            ],
+            'short_description' => [
+                'type' => 'TEXT',
+                'null' => true,
+                'constraint' => 255,
+                'default' => null,
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('blog');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('blog');
+    }
+}
